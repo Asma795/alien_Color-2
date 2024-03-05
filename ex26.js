976a43301fb5a116806e448b45d6e6a481258dc3
Asma Shahid <asmashahid795@gmail.com>
2024-03-06 00:40:13 +0500

    Add files via upload

---
+++ ex26.js
@@ -0,0 +1,19 @@
+// choose a color for an alien as yoy did in ex25, and write in if else chain
+var alienColors = "green";
+// write in if else chain
+// if the alien colors green
+// print the statement that the player just earned 5 points for shooting the alien.
+if (alienColors === "green") {
+    console.log("player just earned 5 points.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
+// if the alien colors is not green print the statement that the player just earned 10 points.
+alienColors = "white";
+if (alienColors === "green") {
+    console.log("player just earned 5 points.");
+}
+else {
+    console.log("player just earned 10 points.");
+}
